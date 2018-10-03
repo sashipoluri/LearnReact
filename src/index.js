89620f5cbf27dfbd5470058e3b368c669a3394e3
sashipoluri <10782155+sashipoluri@users.noreply.github.com>
2018-10-03 11:57:36 -0700

    updated example

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-function App() {
+function Hello(props) {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>testing with git hub</h2>
+      <h2>testing with git hub at time {props.now}</h2>
     </div>
   );
 }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Hello now={new Date().toISOString()} />, rootElement);
